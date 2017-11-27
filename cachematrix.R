8bc42f82da28854253286cd9c29b1ab89c4f2410
unknown <bperro03@gmail.com>
2017-11-26 20:34:23 -0500

    Edited cachematrix.R file with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Creating the makeCachematrix function which creates a list of functions that tdo the following:
+  #set the value of the matrix
+  #get the value of the matrix
+  #set the value of the inverse matrix
+  #get the value of the inverse matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCachematrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  } 
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+#Creating the cacheSolve function which returns the inverse of the matrix created by the makeCachematrix function. 
+#If the inverse has already been calculated, this function will return the cached value, otherwise it will calculate
+#the inverse matrix and store it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+#Testing the cacheSolve function.
+
+z <- matrix(c(1,2,3,4), 2, 2)
+
+cacheSolve(makeCachematrix(z))
